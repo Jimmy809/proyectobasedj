c6444ecfbc496cd4c3e99e8da891c4f3b28feaa0
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-03-16 18:08:14 +0100

    Aplicacion user, models y managers

---
+++ applications/users/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+
+# 
+from .managers import UserManager
+
+# Create your models here.
+
+# con esta class vamos a reutilizar la base de django sobre user, agregandole mas parametros y permisos a los users
+class User(AbstractBaseUser, PermissionsMixin):
+    # aqui declaramos la seleccion que va a tener la variable genero, por el choices agregado
+    GENDER_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+        ('O', 'Otros'),
+
+    )
+    # aqui creamos los campos de la base de datos
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField()
+    nombres = models.CharField(max_length=30, blank=True)
+    apellidos = models.CharField(max_length=30, blank=True)
+    genero = models.CharField(max_length=1, choices = GENDER_CHOICES, blank=True)
+    codregistro = models.CharField(max_length=6, blank=True)
+    
+    # todo aquel q se registre por definicion no sera staff (False)
+    is_staff = models.BooleanField(default=False)    
+    is_active = models.BooleanField(default=False)
+    
+    # necesitamos decirle a django con que se hara el login
+    USERNAME_FIELD = 'username'
+    
+    # para que la consola nos pida el email ya que es obligatorio para crear el usuario
+    # despues de la , podemos seguir agregando parametros para q nos lo pida por consola y sea de manera obligatoria
+    REQUIRED_FIELDS = ['email',] 
+    
+    objects = UserManager()
+
+    # funcion para nombre corto
+    def get_short_name(self):
+        return self.username
+    
+    # funcion para nombre largo, concatenemos nombres y apellidos
+    def get_full_name(self):
+        return self.nombres + ' ' + self.apellidos
